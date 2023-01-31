c353f6906debd6630fd662c71e6af78c5f271fad
Isabela Silva <72450422+Isabela-Silva@users.noreply.github.com>
2023-01-31 13:25:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function mandaZap(elemento) {
+    let texto = elemento.firstElementChild.innerText;
+    let numero = prompt("Qual é o número?");
+    let zapLink = `https://api.whatsapp.com/send?phone=55${numero}&text=${texto}`;
+
+    window.open(zapLink);
+}
